73be784b28c832871d48905cba7ad6296d7e843b
Soleil <soleilherring@gmail.com>
2024-01-08 14:31:04 +0000

    daily coding practice

---
+++ 1480-running-sum-of-1d-array.py
@@ -0,0 +1,43 @@
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+# Return the running sum of nums.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+# Example 2:
+
+# Input: nums = [1,1,1,1,1]
+# Output: [1,2,3,4,5]
+# Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+# Example 3:
+
+# Input: nums = [3,1,2,10,1]
+# Output: [3,4,6,16,17]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 1000
+# -10^6 <= nums[i] <= 10^6
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        # do sliding window approach
+        # get window
+        # lenght of window = 2
+        
+        n = len(nums)
+        k = 2
+        windowSum = sum(nums[:k])
+        new_array = [nums[0], windowSum]
+
+        for i in range(n-k):
+            windowSum = windowSum + nums[i+k]
+            new_array.append(windowSum)
+        return new_array 
+            
