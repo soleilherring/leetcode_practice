13e5445c5025e3672ec3416471ca394b83844a87
Soleil <soleilherring@gmail.com>
2024-01-19 15:56:05 +0000

    daily coding practice

---
+++ code-wars-what-is-between.py
@@ -0,0 +1,13 @@
+# DESCRIPTION:
+# Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+# For example:
+
+# a = 1
+# b = 4
+# --> [1, 2, 3, 4]
+
+def between(a,b):
+    
+    list_range = [i for i in range(a, b+1)]
+    return list_range
