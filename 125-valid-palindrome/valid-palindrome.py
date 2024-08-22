b007035724824d71c753190e0eda65756ba5b1ea
soleilherring <31334083+soleilherring@users.noreply.github.com>
2024-08-22 17:02:13 +0100

    Time: 53 ms (18.97%) | Memory: 21.8 MB (7.19%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,29 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # use two pointer method
-        # get left and right pointer
-        l = 0
-        r = len(s) - 1
+        # two pointers
+        # i = 0 
+        # j = len(s) - 1 
+        # print(s)
+        # while i < j:  
+        #     while i < j and not s[i].isalnum():
+        #         i += 1
+        #     while i < j and not s[j].isalnum():
+        #         j -= 1
+        #     if s[i].lower() != s[j].lower():
+        #         return False
+        #     i += 1 
+        #     j -= 1
+        # return True 
+            
+        ######### other approach, get the string, remove spaces/non alpha numeric characters, compare to its reverse
+        # list comprehension, join
+        letters = ''.join([letter.lower() for letter in s if letter.isalnum()])
+        return letters == letters[::-1]
 
-        # make a while loop to move through the array
-        # while the left pointer is less than the right
-
-        while l < r:
-            # if the letter in the array is alphanumeric, then check when  it is lowercase. 
-            # it the lowercase arent the same  then, return false. Otherwise, keep moving
-            if s[l].isalnum() and s[r].isalnum():
-                # check if when lowercase if they aren't the same, return false and leave loop
-                if s[l].lower() != s[r].lower():
-                    return False
-                # otherwise, we want to keep moving through the array 
-                l += 1
-                r -= 1
-            # else, if the left letter isn't alphanumeric, we want to move left over
-            elif not s[l].isalnum():
-                l += 1 
-            # else, if the right letter isn't alphanmeric, we want to mover right over 
-            else:
-                r -= 1
-        # if we have moved through the entire array and now the left and right are at same spot, 
-        # we can return True because that means we have exited the loop
-        return True 
+        
