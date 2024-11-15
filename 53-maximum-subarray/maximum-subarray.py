47d3420e753468185f9bc1fed509ff2699bd9d9e
soleilherring <31334083+soleilherring@users.noreply.github.com>
2024-11-15 14:24:08 -0800

    Time: 51 ms (73.60%) | Memory: 31.4 MB (6.90%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,32 +1,29 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        '''
-        sliding window?
-        PREP
-        parameters = nums list int
-        return = sum of the numbers
-        example: [-2, 1, -3, 4. -1, 2, 1, -5, 4] --> 4, -1, 2, 1 sum = 6
-        pseudocode:
-        curr_sum = nums[0]
-        max_sum = 0 
-        for num in nums:
-            if curr_sum < 0 :
-                curr_sum = 0
-            curr_sum += num 
-            max_sum = max(max_sum, curr_sum)
-        return max_sum 
-        '''
         
+        '''
+        PREP
+        parameters:
+        int lst of positive and negative numbers
+        return:
+        int largest sum of a subarray
+        example:
+        - nominal:
+        [-2,1,-3,4,-1,2,1,-5,4] -> 6 
+        - edge case:
+        [1] -> 1
+        pseudo:
+        1. variable. tohold the maximum 
+           variable to have the current sum
+        2. if the current sum ever becomes equal to zero, restart the current sum
+        3. compare the maximum sum to the current sum
+        4. return the maximum sum
+        '''
+        max_sum = float('-inf')
         curr_sum = 0
-        max_sum = nums[0]
-
-        for num in nums: #5, 4,-1, 7, 8
-            if curr_sum < 0:
+        for num in nums:
+            if curr_sum <= 0:
                 curr_sum = 0
-            curr_sum += num #23
-            max_sum = max(max_sum, curr_sum) #23
-        return max_sum 
-
-        # [5,4,-1,7,8]
-        # curr_sum = 0
-        # max_sum = 0
+            curr_sum += num
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
