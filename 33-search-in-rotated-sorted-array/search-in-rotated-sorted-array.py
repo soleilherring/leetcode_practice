7dc41c4703e6e7f5d5100ef02244b217bab2bf61
soleilherring <31334083+soleilherring@users.noreply.github.com>
2024-11-19 19:22:56 -0800

    Time: 0 ms (100.00%) | Memory: 17 MB (19.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,30 +1,48 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        '''
+        PREP
+        parameters:
+        int nums list
+        return: index of the target value
+        -1 if the value isn't in the array
+        examples:
+        [4,5,6,1,2] ; 1 -> 3
+        [1,2,3,0,-1] ; 4 -> -1 
+        [1]; 0 -> -1
+        pseudocode:
+        1. create variables to hold the starting and ending index
+            start = 0 
+            end = len(nums) -1
+        2. while loop start < = end 
+            mid = start + end //2 
+
+            # find the pivot
+           
+         return -1
+        '''
         start = 0
         end = len(nums) - 1
 
-        # start binary search
         while start <= end:
-            middle = start + (end - start) // 2
-            if nums[middle] == target:
-                return middle 
-            # check start and middle to see if the array is rotated in first half
-            if nums[start] <= nums[middle]:
-                # check if first part is sorted
-                # if it is, the target is within that section of the array 
-                if nums[start] <= target < nums[middle]:
-                    # limiting search 
-                    end = middle - 1
+            mid = (start + end) // 2
+            # check if the target is equal to the nums[mid]
+            if target == nums[mid]:
+                return mid
+            # check left half to be sorted
+            if nums[start] <= nums[mid]:
+                # check if the target number is in the current range, dont include nums[mid] because we check that
+                # with the first if statement
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
                 else:
-                    start = middle + 1
+                    start = mid + 1
+            # if left half isnt sorted than right half must be
             else:
-                # checking if second half is sorted
-                # if it is, the target is within that section of the array
-                if nums[middle] < target <= nums[end]:
-                    start = middle + 1
+                # check if target is between the nums[mid] and including nums[end], because we did the check of target == nums[mid] with the first check
+                if nums[mid] < target <= nums[end]:
+                    start = mid + 1
                 else:
-                    end = middle -1 
+                    end = mid - 1
         return -1 
-
-
       
