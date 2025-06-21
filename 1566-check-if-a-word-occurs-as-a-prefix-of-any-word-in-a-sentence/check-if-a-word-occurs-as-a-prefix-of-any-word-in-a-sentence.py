8ca8d873b45319aab4b6e5737194bbdbaeb65ee0
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-06-21 22:15:21 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (29.58%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        '''
+        parameters:
+        string of words -> sentence
+        string of characters -> searchWord
+        
+        return 
+        index + 1 of the word that has the searchWord as a prefix
+    
+        example:
+        "we love to eat tacos"
+        ea
+        return 4
+
+        if searchword not in sentence, return 
+        -1
+
+        pseudocode
+        split sentence into words
+        window of size searchWord
+        if searchWord == window for each word, return it's index + 1
+        '''
+
+        split_sentence = sentence.split(" ")
+        k = len(searchWord)
+        
+        for index, word in enumerate(split_sentence):
+            if len(word) < k:
+                continue
+            if word[:k] == searchWord:
+                return index + 1
+        return -1
