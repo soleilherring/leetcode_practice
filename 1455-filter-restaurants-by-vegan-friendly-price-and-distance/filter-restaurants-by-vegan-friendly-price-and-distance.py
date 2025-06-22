6c6cb56ca0d54345d6ff48bb727b46fc0e1141b9
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-06-22 20:10:01 +0100

    Time: 4 ms (56.54%) | Memory: 24.6 MB (96.86%) - LeetSync

--- 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.py
+++ 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.py
@@ -15,13 +15,26 @@ class Solution:
         # for restaurant in restaurants:
             # lambda function
             # if veganFriendly = 1, filter out restaurants that are vegan friendly
-        if veganFriendly == 1:
-            restaurants = list(filter(lambda restaurant: restaurant[2] == 1, restaurants))
-            # otherwise filter based on maxPrice, maxDistance
-            # filter to check that the maxPrice for each restaurant is lower than the maxPrice variable
-        restaurants = list(filter(lambda restaurant: restaurant[3]<= maxPrice and restaurant[4] <=maxDistance, restaurants))
-        # sorty by rating (descending)
-        sorted_restaurants = sorted(restaurants, key = lambda r:(-r[1], -r[0]))
+        # if veganFriendly == 1:
+        #     restaurants = list(filter(lambda restaurant: restaurant[2] == 1, restaurants))
+        #     # otherwise filter based on maxPrice, maxDistance
+        #     # filter to check that the maxPrice for each restaurant is lower than the maxPrice variable
+        # restaurants = list(filter(lambda restaurant: restaurant[3]<= maxPrice and restaurant[4] <=maxDistance, restaurants))
+        # # sorty by rating (descending)
+        # sorted_restaurants = sorted(restaurants, key = lambda r:(-r[1], -r[0]))
        
-        return [r[0] for r in sorted_restaurants]
+        # return [r[0] for r in sorted_restaurants]
         
+        # make constants
+    
+        ID, RATING, VEGAN, PRICE, DIST = 0, 1, 2, 3, 4
+        filtered = []
+        for restaurant in restaurants:
+            if ((not veganFriendly or restaurant[VEGAN] ==1) 
+                and restaurant[PRICE] <= maxPrice 
+                and restaurant[DIST] <= maxDistance):
+                filtered.append(restaurant)
+        
+        sorted_restaurants = sorted(filtered, key=lambda r: (r[RATING], r[ID]), reverse=True)
+
+        return [r[ID] for r in sorted_restaurants]
