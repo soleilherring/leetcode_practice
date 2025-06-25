f8925a35595a7a1c64fe12f742b00ce67338af8b
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-06-25 21:52:15 +0100

    Time: 6 ms (49.63%) | Memory: 19 MB (47.23%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        """
+        PREP
+        parameters: string 
+        return
+        list of strings
+        example:
+        [a1B2, a1B2, A1b2, A1B2]
+        pseudocode
+        1. get the 
+    
+        """
+        final = []
+        result = ""
+        # dfs backtracking
+        def dfs(index, result):
+            #  base case
+            if index >= len(s):
+                return final.append(result)
+            # let\ter 
+            if s[index].isalpha():
+                # lowercase
+                dfs(index + 1, result + s[index].lower())
+                # uppercase 
+                dfs(index + 1, result + s[index].upper())
+            # number (just append it)
+            else:
+                dfs(index + 1, result + s[index])
+        
+        dfs(0,"")
+        return final
