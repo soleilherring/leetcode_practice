35980ab59a57ed89e071a5b772b04cdfd103d1b6
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-06-29 22:17:24 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (9.33%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -10,12 +10,21 @@ class Solution:
     # 3. for each key in dictionary 
     #       check if it has a value. if not, return key 
 
-        dest_dict = {}
-        for path in paths:
-            dest_dict[path[0]] = path[1]
+        # dest_dict = {}
+        # for path in paths:
+        #     dest_dict[path[0]] = path[1]
 
-        for path in dest_dict:
-            while path in dest_dict:
-                path = dest_dict[path]
-        return path
+        # for path in dest_dict:
+        #     while path in dest_dict:
+        #         path = dest_dict[path]
+        # return path
         
+        start = set()
+        end = set()
+        for path in paths:
+            if path[0] not in start:
+                start.add(path[0]) #{london, new york,lima }
+            if path[1] not in end:
+                end.add(path[1]) #{new york, lima, san paulo}
+        return (end-start).pop()
+            # 
