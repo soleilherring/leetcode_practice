0fec04fa8e452949d17988f0c61eafdf26e9186f
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-06 18:32:34 +0100

    Time: 16 ms (67.74%) | Memory: 17.8 MB (52.81%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,23 +1,26 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # sliding window
-        # make a set to hold unique values
-        # variable to hold max count of chars so far
-        # make left pointer
-        # for loop to iterate through len of list
-        # while loop to check if the letter is in the set
-        # if in the set remove the letter from the set and increment left pointer 
-        # if not in set, add it to set
-        # calculate the max count so far by comparing our current max count and the index of r - l + 1. We do this because we want to find the difference in length, which gives us how many characters, but add one since we want it to be inclusive of our index
-        # return the max
+        # 'a b c a b c b b'
+        #                s
+        #                r 
+        # set = {a}
+        # max_length = s:r+ 1 -> 3
+        # # if the letter is in the set, remove it from the set and adjust the start
 
-        unique = set()
-        max_count = 0
+        # 'p w w k e w'
+        #      l
+        #            r  
+        # set { w k e}
+        # max substring so far : s[l : r ] => 3
+        
         l = 0
-        for r in range(len(s)):
-            while s[r] in unique:
+        unique = set()
+        max_length = 0
+        
+        for right in range(len(s)):
+            while s[right] in unique:
                 unique.remove(s[l])
-                l += 1 
-            unique.add(s[r])
-            max_count = max(max_count, r - l + 1)
-        return max_count 
+                l+= 1
+            unique.add(s[right])
+            max_length = max(max_length, len(unique))
+        return max_length
