944e8c5115e797c887addcce3a7fdd2ba3434845
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-07 20:14:03 +0100

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.17%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,34 +1,44 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        """
-        parameters:
-        list of strings
-        return: 
-        array of the k most frequent strings in lexicographical order
-        pseudo:
-           [ ,   ,[i, love],  ]
-            0  1. 2
+        # parameter: 
+        # array of strings
+        # int k (number of most frequent words)
+        # return:
+        # array of k most frequent words
+        # - note to sort by lexicographical order if needed
+        # - descending order otherwise
 
-        1. dictionary 
-        2. list of lists that occurs the same number of the len of words 
-        3. for loop for each word in the dictionary 
-            add it's count as the position in the above array 
-        4. loop backwards as long as the len of the array > 0. and do it up to k times or while len of the array < len k
-    
-        """
-        counter = Counter(words)
-
-        frequency = [[] for word in range(len(words) + 1)]
-
-        for word, freq in counter.items():
-            frequency[freq].append(word)
+        # 1. declare /initialize a counter dictionary
+        # 2. if the value (count) is the same for k top number of words, sort lexicographically
+        # otherwise, return 
+        count_words= {}
+        for word in words:
+            count_words[word] = count_words.get(word, 0) + 1 
         
+        max_count = 0
+        max_word = []
+        freq = [ [] for _ in range(len(words)+1)]
+
+        for word, count in count_words.items():
+            # cat, dog, dog, rover, rover
+            # [[], [cat], [dog, rover], [],[],[]]
+            #    0.    1.     2
+            
+            freq[count].append(word)
+        # move backwards through the freq array and skip if empt
+        # check if the array len is same as k, if so sort and get the array
+            # otherwise continue grabbing the words within the arry and add to result array 
+
         result = []
-        for words in range(len(frequency) -1, 0, -1) :
-            if not len(frequency[words]):
-                continue 
-            for word in sorted(frequency[words]):
-                result.append(word)
-                if len(result) == k :
-                    return result
+        for i in range(len(words) -1, 0, -1):
+            if freq[i]:
+                for word in sorted(freq[i]):
+                    result.append(word)
+                    if len(result) == k:
+                        return result
+
+
+
+
+                
 
