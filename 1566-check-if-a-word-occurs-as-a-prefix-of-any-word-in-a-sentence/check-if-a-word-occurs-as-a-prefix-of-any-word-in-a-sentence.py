e29defaae0e2b53aa948a9dac7b92e9234d84fdd
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-08 20:46:56 +0100

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.59%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,30 +1,21 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        '''
-        parameters:
-        string of words -> sentence
-        string of characters -> searchWord
-        
-        return 
-        index + 1 of the word that has the searchWord as a prefix
-    
-        example:
-        "we love to eat tacos"
-        ea
-        return 4
+        # parameter: 
+        # string sentence with  spaces
+        # string with a prefix 
+        # return the index + 1 of the prefix word in the string ,only can be at start
+        # example: "she eats cherry and peachey pies" searchWord = 'che'
+        # psuedo:
+        # 1. split the string into words within an array separated by a comma
+        # 2. use the search word to check the first len(searchword) positions in the word,
+        #     if the searchword == word[:len(searchword)]
+        #         return the index + 1 //because 1 indexed
+        #  3. return -1 //no prefix in string
 
-        if searchword not in sentence, return 
-        -1
-
-        pseudocode
-        split sentence into words
-        window of size searchWord
-        if searchWord == window for each word, return it's index + 1
-        '''
-
+        window = len(searchWord)
         split_sentence = sentence.split(" ")
 
         for index, word in enumerate(split_sentence):
-            if word.startswith(searchWord):
+            if word[:window] == searchWord:
                 return index + 1
-        return -1
+        return -1 
