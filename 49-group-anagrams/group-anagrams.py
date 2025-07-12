4db3a31ff8f8742dd9516dab204049826eb594c9
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-12 16:53:55 +0100

    Time: 15 ms (49.90%) | Memory: 22.7 MB (26.06%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,47 +1,33 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        '''
-        PREP
-        parameters:
-        array of strings
-        return:
-        list of lists with anagrams grouped together
-        example:
-        ["can", "nap", "pan", "lap", "now", "pal"]
-        [["can"], ["nap", pan], ["now"], ["lap", "pal"]]
-        pseudo:
-        1. make a dictionary 
-           make an array to hold the count of each letter at that index (index 0 is letter a, the count of  'a' chars would be there)
-           use ord()
-        for each word count how many times the letters appear
-        2. in result dictionary, have the tuple(count) as the key and the word list is the value. 
-        '''
-        result = defaultdict(list)
-        # m = number of strings
-        # n = lenth of each str
-        for word in strs: 
-            count = [0] * 26
-            for letter in word:
-                count[ord(letter) - ord('a')] += 1
-            result[tuple(count)].append(word)
-        return list(result.values())
+        # make an array with the 26 letters in. thedictionary 
+        # we will count the occurence of each letter and put it in the array
+        # [0, 0]
+        #  a. b...
+        # #  make these into a tuple then set them as the key to a dictoanry
+        #      if a word when made into this occurence array is in the dictionary, has this many letters 
+        #         it will be the value (append the word the result value )
+        # return the result 
 
-        # Time: O(m * n) 
-        # Space: O(m * n) space complexity because in the worst case each word is unique and we have list of lists containig all strings
+        # create frequency array
 
-# old version
-        # make a dictionary to hold the words
-        # interate through the list of words and sort the letters in the word
-        # if the letter sorted in the word is already in dictionary, add the unsorted word to dictionary if not, add just the word 
-        # # return list of words
-        # if not strs:
-        #     return [strs]
-        # # word is key and value will default to list
-        # anagram_dict = defaultdict(list)
+        anagrams = defaultdict(list)
 
-        # for word in strs:
-        #     sorted_word = "".join(sorted(word))
-        #     anagram_dict[sorted_word].append(word)
         
-        # return list(anagram_dict.values())
+        for word in strs:
+            # calcuated for that word the number of times its letters occur
+            freq = [0] * 26
+            for letter in word:
+                freq[ord(letter) - ord('a')] += 1
+
+            # tuples are immutable so convert list into tuple and make it a key in the dict
+            key = tuple(freq)
+            # if key in anagrams:
+            #     anagrams[key].append(word)
+            # else:
+            #     anagrams[key] = [word]
+            anagrams[key].append(word)
+        return list(anagrams.values())
+
+        
