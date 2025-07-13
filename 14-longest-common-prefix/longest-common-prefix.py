e4ff749f367a7fd6623bfa39379639a144e2f681
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-13 13:29:10 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.87%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # # parameter 
+        # list of strings
+        # return the lonest common prefix
+        # example:
+        # 'carpet', 'car', 'carpenter' -> car 
+        # psuedocode:
+        # 1. find the shortest word in the string array
+        # 2. for each word in strs, move through each word and see if it mathes the letters in the stortest                                                                     
+        # word 
+        # 3. save the longest prefix in another variable
+        # 4. when we hit the longest substring break out of the loop and return it. 
+
+        # min 
+        shortest = min(strs, key = len)
+
+        for i in range(len(strs)):
+
+            while not strs[i].startswith(shortest):
+                shortest = shortest[:-1]
+        return shortest
+    
