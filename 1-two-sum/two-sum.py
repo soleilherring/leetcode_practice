7c3cef9f133bea1a69185a83bb3bf49a55ade344
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-17 20:12:42 +0100

    Time: 3 ms (50.43%) | Memory: 18.7 MB (65.07%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,20 +1,60 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # parameters:
-        # int list of nums
-        # int target 
-        # return array of indexes
-        # example: [2,3,5] target = 8, return: [1,2]
-        # [2,2] target = 4 ! return [0,0] =>[0,1]
+        # parameter
+        # 1. list of nums
+        # 2. target
 
-        # psuedo:
-        # array of nums where each index 
-        num_dict = {}
+        # return the indices of the two numbers such that they add up to target [index1, index2]
+        # example:
+        # [1, 2, 4, 3] target = 6
+        # [1,2]
+
+        # - cannot reuse the same index twice, one solution
+
+        # pseudocode:
+        # {
+        #     1:0, 2:1, 4:2, 3:3
+        # }
+        # difference = 6-1 = 5 
+        # difference = 6- 2 = 4
+        # retun [1, 2]  
+
+        # a. dict key = num, val - index
+        # b. loop through each num in array
+        #     calculate the difference between the target and n um
+        #     if the difference is in the num_index, retrun its value and the current value (index)
+
+        num_index = {}
+
         for index, num in enumerate(nums):
             difference = target - num
-            if difference in num_dict:
-                return [index, num_dict[difference]]
-            num_dict[num] = index  
-        return []
+            if difference in num_index:
+                return [index, num_index[difference]]
+            num_index[num] = index 
+
+
+
+
+
+
+
+
+
+        # # parameters:
+        # # int list of nums
+        # # int target 
+        # # return array of indexes
+        # # example: [2,3,5] target = 8, return: [1,2]
+        # # [2,2] target = 4 ! return [0,0] =>[0,1]
+
+        # # psuedo:
+        # # array of nums where each index 
+        # num_dict = {}
+        # for index, num in enumerate(nums):
+        #     difference = target - num
+        #     if difference in num_dict:
+        #         return [index, num_dict[difference]]
+        #     num_dict[num] = index  
+        # return []
 
             
