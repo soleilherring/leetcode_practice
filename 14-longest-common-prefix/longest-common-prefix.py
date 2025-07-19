5373df85e59869713754c0a5b088655cf24fcfe3
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-19 20:07:41 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (30.52%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,5 +1,55 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        # parameters:
+        # array of strings
+        # output is longest common prefix
+
+        # find the shortest length word
+
+        # for every letter in that shortest length word
+        # check if it each letter in shortest appears in the word
+        # once it doesnt, return it 
+
+        shortest  = sorted(strs, key = len)[0]
+        print(shortest)
+
+        for i in range(len(shortest)):
+            for word in strs:
+                if shortest[i] != word[i]:
+                    shortest = word[:i]
+                    return shortest
+        return shortest
+        print(prefix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # # parameter 
         # list of strings
         # return the lonest common prefix
@@ -12,23 +62,23 @@ class Solution:
         # 3. save the longest prefix in another variable
         # 4. when we hit the longest substring break out of the loop and return it. 
 
-        # min 
-        shortest = min(strs, key = len)
+        # # min 
+        # shortest = min(strs, key = len)
 
-        # for i in range(len(strs)):
+        # # for i in range(len(strs)):
 
-        #     while not strs[i].startswith(shortest):
-        #         shortest = shortest[:-1]
-        # return shortest
+        # #     while not strs[i].startswith(shortest):
+        # #         shortest = shortest[:-1]
+        # # return shortest
 
-        # second way
+        # # second way
 
-        for i in range(len(shortest)):
-            for word in strs:
-                if word[i] != shortest[i]:
-                    shortest = shortest[:i]
-                    return shortest
-        return shortest #add this so in case there are never mismatches and the entire prefix is in the word
+        # for i in range(len(shortest)):
+        #     for word in strs:
+        #         if word[i] != shortest[i]:
+        #             shortest = shortest[:i]
+        #             return shortest
+        # return shortest #add this so in case there are never mismatches and the entire prefix is in the word
        
 
 
