768e1540a0b133564d18f1369723b9471eafd9fc
soleilherring <31334083+soleilherring@users.noreply.github.com>
2025-07-19 18:23:15 +0100

    Time: 15 ms (41.80%) | Memory: 18.1 MB (33.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,69 +1,78 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        '''
-        PREP
-        parameters:
-        s string
-        t string
-        return:
-        true or false
-        true if anagram(letters in different order can make same word)
-        example
-        "anagram" ; "nagram" -> true
-        "rat" ;"car" -> false
-        "sl" ; "s" -> false
-        ''; '' -> true
-        pseudo:
-        1. create a hashmap
-        have a count of the letter in one string
-        have a deduction of the letter of the same character in the other string
-        if they become zero, than we found the same number of strings in both string
-        2. if there is a non zero value in the dictionary, that means one or more of the characters appears 
-        multiple times, so they are not anagrams
-            return  false
-        '''
-        # if len(s) != len(t):
-        #     return False
 
-        # s_count = {}
-        # for i in range(len(s)):
-        #     s_count[s[i]] = s_count.get(s[i], 0) + 1
-        #     s_count[t[i]] = s_count.get(t[i], 0) - 1
-        # # count the values, and they should cancel each other out if they have the same letters and are anagrams
-        # # otherwise, if they are any extra letters the value of the dictionary will not be 0 and we will return false
+        # paramsters:
+        # two strings
+        # s 
+        # t
+
+        # return true or false
+
+        # for each letter, check if it is in the other word's dictionary. if it is, subtract that letter from the dictionary
+        # at the end check if the dictionary has any non 0 value, if so return false
+
+        # or
+        if len(t) != len(s):
+            return False
+
+        letters_in_s = Counter(s)
+
+        for letter in t:
+            if letter in letters_in_s:
+                letters_in_s[letter] -= 1
+            
+
+        for count in letters_in_s.values():
+            if count != 0:
+                return False
+        return True 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # parameters: 
+        # s string of chars
+        # t string of chars
+        # return boolean (true if t is anagram of s)
+        # 1. check if they are of different lengths, return false
+        # 2.  sort and then compare?
+        # 3. if they are the same, return true otherwise false
+        # # comapre the sorted viersions
+        # return sorted(s) == sorted(t)/\
+
+        # dictionary for s with key as letter and value as count. loop through t and deduct when it's in the dct andd if lterr is not in dict return false
+
+        # s_count = Counter(s)
+
+        # for letter in t:
+        #     if letter not in s_count:
+        #         return False
+        #     s_count[letter] -=1
+
         # for val in s_count.values():
         #     if val != 0:
         #         return False
-        # return True 
+        # return True
 
-        #################### version two using ord()
-        '''
-        pseudo
-        1. have an array with 26 spots for each letter in the alphabet
-            assume the letters are lowercase in this instance
-        2. for each letter in s find it's ord number by subtracting its ord number from letter a
-        this will tell us the index postion to put the count at
-            example:
-            x = 'z'
-            count = 26 * [0]
-            for x in s:
-                count[ord(x) - ord('a')] += 1 
-                *** same as count[122 - 97] += 1
-                            count[25] += 1
-                            [0.......1]
-        3. loop through the count
-            if there are any values !=0, return false, otherwise true
-
-        '''
-        count = [0] * 26 #making. anarray with 26 positions with 0 as the frequency at that position
-        for letter in s:
-            count[ord(letter) - ord('a')] += 1 #subtract the unicode number from a, since only 26 letters in alphabet will get a index to place the frequency of the letter in the array
-
-        for letter in t:
-            count[ord(letter) - ord('a')] -= 1 #finding the unicode number again to find the count of letter in the count aray and then deducting it
-
-        for frequency in count: #since the anagram is assumed to have the same number of letters we should cancel them out by subtracting, them. if not 0, its false
-            if frequency != 0:
-                return False
-        return True
-
+        
+        
