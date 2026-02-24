b99fd479bb97c0b0c4ed0dfb557f9c324c27e52e
soleilherring <31334083+soleilherring@users.noreply.github.com>
2026-02-24 21:10:42 +0000

    Time: 1 ms (41.82%) | Memory: 56 MB (20.00%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.ts
+++ 1944-truncate-sentence/truncate-sentence.ts
@@ -1,5 +1,5 @@
 function truncateSentence(s: string, k: number): string {
-    const result = s.split(" ").slice(0,k).join(' ')
-    return result
+    return s.split(" ").slice(0,k).join(' ')
+  
 
 };
